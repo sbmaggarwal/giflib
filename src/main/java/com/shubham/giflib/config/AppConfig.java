63f2ff163cb69f6d93ba8c1cb93c8d376e9f9827
Shubham Aggarwal <sbmaggarwal@gmail.com>
2017-01-12 16:39:21 +0530

    Added messages.properties file to show custom validation and flash messages

--- src/main/java/com/shubham/giflib/config/AppConfig.java
+++ src/main/java/com/shubham/giflib/config/AppConfig.java
@@ -2,20 +2,47 @@ package com.shubham.giflib.config;
 
 import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @PropertySource("app.properties")
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private Environment env;
 
     @Bean
     public Hashids hashids() {
         return new Hashids(env.getProperty("giflib.hash.salt"),8);
+    }
+
+    //below 2 beans are for loading custom validation and flash messages
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        // include file as messages.properties
+        bean.setBasename("classpath:messages.properties");
+        bean.setDefaultEncoding("UTF-8");
+        return bean;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
     }
 }
